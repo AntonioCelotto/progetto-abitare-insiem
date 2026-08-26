38ea04113dbd6fc2b1b65f349376264a4be81591
AntonioCelotto <a.celotto@newdigitalapp.com>
2026-08-26 13:44:42 +0200

    Add application layout

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Progetto Abitare Insieme',
+  description: "Dall'idea all'apertura della tua struttura per anziani.",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="it">
+      <body>{children}</body>
+    </html>
+  );
+}
